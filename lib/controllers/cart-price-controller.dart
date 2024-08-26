9fec4e5102f5223239142ee0460c440f8f2a5ba7
sohaib nawaz <sohaibnawaz2000@gmail.com>
2024-08-26 12:19:09 +0500

    cart total price calculation done

--- lib/controllers/cart-price-controller.dart
+++ lib/controllers/cart-price-controller.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, unnecessary_null_comparison
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
